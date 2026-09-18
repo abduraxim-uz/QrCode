254c72840e67ee05b10aef24747d03481489f08a
Abdurakhim <abdurakhim.tursunboyev@gmail.com>
2026-09-18 17:40:35 +0500

    Bugs fixed

--- src/main/java/uz/abduraxim/qrcode/controller/QrCodeController.java
+++ src/main/java/uz/abduraxim/qrcode/controller/QrCodeController.java
@@ -4,14 +4,14 @@ import com.google.zxing.BarcodeFormat;
 import com.google.zxing.client.j2se.MatrixToImageWriter;
 import com.google.zxing.common.BitMatrix;
 import com.google.zxing.qrcode.QRCodeWriter;
+import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 import uz.abduraxim.MyLogger;
-import uz.abduraxim.qrcode.util.StartUp;
 
 import java.awt.image.BufferedImage;
 
@@ -21,10 +21,8 @@ import static org.springframework.http.MediaType.IMAGE_JPEG_VALUE;
 @RequiredArgsConstructor
 public class QrCodeController {
 
-    private final StartUp startup;
-
-    @GetMapping(value = "/qr/{text}", produces = IMAGE_JPEG_VALUE)
-    public ResponseEntity<BufferedImage> generateQRCode(@PathVariable String text) throws Exception {
+    @GetMapping(value = "/qr", produces = IMAGE_JPEG_VALUE)
+    public ResponseEntity<BufferedImage> generateQRCode(@RequestParam String text) throws Exception {
         MyLogger.info("User text: " + text);
         QRCodeWriter qrCodeWriter = new QRCodeWriter();
         BitMatrix bitMatrix = qrCodeWriter.encode(text, BarcodeFormat.QR_CODE, 200, 200);
@@ -33,8 +31,15 @@ public class QrCodeController {
     }
 
     @GetMapping("/qr/start")
-    public String generateQRCodePage(Model model) {
-        model.addAttribute("baseUrl", "http://localhost:" + startup.getServerPort());
+    public String generateQRCodePage(HttpServletRequest request, Model model) {
+        String scheme = request.getScheme();
+        String serverName = request.getServerName();
+        int port = request.getServerPort();
+        String contextPath = request.getContextPath();
+
+        String baseUrl = scheme + "://" + serverName + ((port == 80 || port == 443) ? "" : ":" + port) + contextPath;
+        MyLogger.info("baseUrl: " + baseUrl);
+        model.addAttribute("baseUrl", baseUrl);
         return "qrcode-page";
     }
 }
